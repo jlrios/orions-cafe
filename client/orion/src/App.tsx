5e735ec62e5775c978f63094af9744f792964c1a
jlrios <rdjl5791@gmail.com>
2024-11-20 14:51:15 -0600

    A React project was created.

---
+++ client/orion/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">El Café de Orión</a>
+        <nav>
+          <a href="">Suscribirse</a>
+          <a href="">Acerca de</a>
+        </nav>
+      </header>
+      <div className="container">
+        Article.
+      </div>
+    </main>
+  )
+}
+
+export default App
